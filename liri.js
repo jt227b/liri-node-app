b67ee35707acd35cbb7bafeaf47508d73b9120da
Jose G Torres <46248532+jt227b@users.noreply.github.com>
2019-04-09 11:24:01 -0500

    Added Var Keys

--- liri.js
+++ liri.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+
+// fs = File System //
+var fs = require("fs");
